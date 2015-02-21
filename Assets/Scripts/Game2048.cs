66aafb8694e41a204d8f733ab880823e153808ed
andrey <andrey@volodko.org>
2015-02-22 01:59:27 +0200

    Fix swipes, android settings

--- Assets/Scripts/Game2048.cs
+++ Assets/Scripts/Game2048.cs
@@ -21,6 +21,7 @@ public class Game2048 : MonoBehaviour
 		private Vector3 downStartPosition;
 		private Vector3 downEndPosition;
 		private Vector3 nullPosition = new Vector3 (0, 0, 0);
+		private Vector2 currentSwipe;
 		public GameObject cellTemplate;
 		public GameObject cellBgTemplate;
 		private int playerScore = 0;
@@ -102,20 +103,32 @@ public class Game2048 : MonoBehaviour
 								mouseLeftDown = false;
 								downEndPosition = Input.mousePosition;
 								if (downStartPosition != nullPosition && downEndPosition != nullPosition) {
-										float deltaX = downStartPosition.x - downEndPosition.x;			
-										float deltaY = downStartPosition.y - downEndPosition.y;	
-										if (deltaX < -5) 
-												calcMovement (MOVE_RIGHT);
-										if (deltaX > 5) 
-												calcMovement (MOVE_LEFT);
-										if (deltaY > 1) 
-												calcMovement (MOVE_DOWN);
-										if (deltaY < -1) 
+					
+										//create vector from the two points
+										currentSwipe = new Vector2 (downEndPosition.x - downStartPosition.x, downEndPosition.y - downStartPosition.y);
+					
+										//normalize the 2d vector
+										currentSwipe.Normalize ();
+					
+										//swipe upwards
+										if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
 												calcMovement (MOVE_UP);
+										}
+										//swipe down
+										if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+												calcMovement (MOVE_DOWN);
+										}
+										//swipe left
+										if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+												calcMovement (MOVE_LEFT);
+										}
+										//swipe right
+										if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+												calcMovement (MOVE_RIGHT);
+										}
 										downStartPosition = nullPosition;
 										downEndPosition = nullPosition;
 								}
-
 							
 						}
 				
